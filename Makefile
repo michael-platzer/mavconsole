c99344def7918a89610cf1ae45b745196355ce38
Michael Platzer <michael.platzer@hotmail.com>
2021-11-26 09:44:44 +0100

    Add argument parsing to event server

--- Makefile
+++ Makefile
@@ -4,4 +4,4 @@ MAVLINK_PATH := $(abspath mavlink/)
 test:
 	PYTHONPATH=$(MAVLINK_PATH) python3 mavlink/pymavlink/tools/mavplayback.py *.tlog &
 	sleep 1
-	PYTHONPATH=$(MAVLINK_PATH) python3 mavevent-server.py
+	PYTHONPATH=$(MAVLINK_PATH) python3 mavevent-server.py --host localhost 127.0.0.1:14550
--- mavevent-server.py
+++ mavevent-server.py
@@ -5,70 +5,104 @@ import threading
 import queue
 import websockets
 import json
+import argparse
+
+
+###############################################################################
+# PARSE SELECT EVENTS FROM MAVLINK STREAM
 
 from pymavlink import mavutil
 
-mavevent_queue = queue.SimpleQueue()
-
-MAVEVENT_MESSAGES = {
+MAVEVENT_PARSE_FUNCS = {
     # https://mavlink.io/en/messages/common.html#GLOBAL_POSITION_INT
-    'GLOBAL_POSITION_INT': lambda msg: {
+    'GLOBAL_POSITION_INT': lambda msg: {'ua_pos': {
         'lat': msg.lat / 10**7, 'lon': msg.lon / 10**7, 'alt': msg.alt / 1000,
         'vlat': msg.vx / 100, 'vlon': msg.vy / 100, 'vz': -msg.vz / 100
-    },
+    }},
     # https://mavlink.io/en/messages/common.html#GLOBAL_POSITION_INT_COV
-    'GLOBAL_POSITION_INT_COV': lambda msg: {
+    'GLOBAL_POSITION_INT_COV': lambda msg: {'ua_pos': {
         'lat': msg.lat / 10**7, 'lon': msg.lon / 10**7, 'alt': msg.alt / 1000,
         'vlat': msg.vx / 100, 'vlon': msg.vy / 100, 'vz': -msg.vz / 100
-    },
+    }},
 }
 
-def mavevent_recv():
-    global mavevent_queue
-    mavconn = mavutil.mavlink_connection('127.0.0.1:14550')
+def mavevent_recv(connection_str, event_queue):
+    mavconn = mavutil.mavlink_connection(connection_str)
     mavconn.wait_heartbeat()
     while True:
-        msg = mavconn.recv_match(type=list(MAVEVENT_MESSAGES.keys()), blocking=True)
+        msg = mavconn.recv_match(
+            type=list(MAVEVENT_PARSE_FUNCS.keys()), blocking=True
+        )
         if not msg or msg.get_type() == 'BAD_DATA':
             continue
-        print(f"Got a {msg.get_type()} message")
-        parse_func = MAVEVENT_MESSAGES.get(msg.get_type(), None)
+        parse_func = MAVEVENT_PARSE_FUNCS.get(msg.get_type(), None)
         if parse_func is not None:
-            mavevent_queue.put(json.dumps(parse_func(msg)))
+            event_queue.put(parse_func(msg))
 
 
-mavevent_connections = set()
+###############################################################################
+# SERVE EVENTS VIA WEBSOCKETS
+
+mavevent_connections = set() # set of active websocket connections
+mavevent_state       = {}    # current state accumulated from prior events
 
 async def mavevent_session(websocket, path):
     '''
     Handle a websocket session for a client waiting for MAVLink events
     '''
-    global mavevent_connections
     mavevent_connections.add(websocket)
     try:
-        # Send previous moves, in case the game already started.
-        #await replay(websocket, game)
-        # wait until the client disconnects (we do not expect to receive any data)
+        # send current state
+        await websocket.send(json.dumps(mavevent_state))
+        # wait until the client disconnects
         await websocket.wait_closed()
     finally:
         mavevent_connections.remove(websocket)
 
-async def mavevent_serve():
+async def mavevent_serve(event_queue, host=None, port=None):
     '''
-    Serve select MAVLink events via websockets
+    Serve events read from a queue via websockets
     '''
-    global mavevent_connections
-    global mavevent_queue
-    async with websockets.serve(mavevent_session, 'localhost', 8000):
+    async with websockets.serve(mavevent_session, host, port):
         while True:
             try:
-                event = mavevent_queue.get_nowait()
-                #websockets.broadcast(mavevent_connections, event)
+                event = event_queue.get_nowait()
+                mavevent_state.update(event)
+                #websockets.broadcast(mavevent_connections, json.dumps(event))
+                event_str = json.dumps(event)
                 for conn in mavevent_connections:
-                    await conn.send(event)
+                    await conn.send(event_str)
             except queue.Empty:
                 await asyncio.sleep(0.001)
 
 
-threading.Thread(target=mavevent_recv, daemon=True).start()
-asyncio.run(mavevent_serve())
+###############################################################################
+# ARGUMENT PARSING AND INITIALIZATION
+
+argparser = argparse.ArgumentParser(description='')
+argparser.add_argument(
+    '--host', nargs='?', default=None,
+    help=(
+        'Network interfaces on which the websocket server listens, '
+        'see https://websockets.readthedocs.io/en/stable/reference/server.html'
+    )
+)
+argparser.add_argument(
+    '-p', '--port', nargs='?', default=8000,
+    help='TCP port on which the server websocket server listens'
+)
+argparser.add_argument(
+    'connection_string', metavar='CONNECTION_STRING',
+    help=(
+        'MAVLink connection string, '
+        'see https://mavlink.io/en/mavgen_python/#connection_string'
+    )
+)
+args = argparser.parse_args()
+
+mavevent_queue = queue.SimpleQueue()
+threading.Thread(
+    target=mavevent_recv, args=(args.connection_string, mavevent_queue),
+    daemon=True
+).start()
+asyncio.run(mavevent_serve(mavevent_queue, args.host, args.port))
